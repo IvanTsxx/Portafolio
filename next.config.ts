49dcfccb985513c3b7e35150a0b59dc5072e93b3
ivan2214 <bongiovanniivan12@gmail.com>
2026-02-07 19:47:11 -0300

    add mdx

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,20 @@
+import createMDX from "@next/mdx";
 import type { NextConfig } from "next";
 import "./env/server";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+  extension: /\.(md|mdx)$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
+});
+
+// Merge MDX config with Next.js config
+export default withMDX(nextConfig);
