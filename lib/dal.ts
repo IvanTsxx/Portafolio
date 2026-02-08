19e6c849d716a56c6f927e3e8748c6f14a66298a
ivan2214 <bongiovanniivan12@gmail.com>
2026-02-08 13:07:21 -0300

    fix: login in admin

--- lib/dal.ts
+++ lib/dal.ts
@@ -1,21 +1,12 @@
 import "server-only";
-import { cookies } from "next/headers";
+import { headers } from "next/headers";
 import { cache } from "react";
 import { auth } from "@/lib/auth";
 
 export const getSession = cache(async () => {
-  const cookieStore = await cookies();
-  const sessionCookie = cookieStore.get("better-auth.session_token");
-
-  if (!sessionCookie) {
-    return null;
-  }
-
   try {
     const session = await auth.api.getSession({
-      headers: {
-        cookie: `better-auth.session_token=${sessionCookie.value}`,
-      },
+      headers: await headers(),
     });
 
     return session;
