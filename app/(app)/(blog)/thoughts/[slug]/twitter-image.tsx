b26ac4182891c2027efe209127601116645091ca
ivan2214 <bongiovanniivan12@gmail.com>
2026-04-08 09:11:40 -0300

    feat: add Twitter image generation by re-exporting existing OpenGraph images

--- app/(app)/(blog)/thoughts/[slug]/twitter-image.tsx
+++ app/(app)/(blog)/thoughts/[slug]/twitter-image.tsx
@@ -1,6 +1,6 @@
 // Twitter cards use the same 1200x630 dimensions
 export const size = { height: 630, width: 1200 };
-export const contentType = "image/webp";
+export const contentType = "image/png";
 
 // Re-export the same OG component for Twitter
 export { default } from "./opengraph-image";
--- app/(app)/(blog)/thoughts/twitter-image.tsx
+++ app/(app)/(blog)/thoughts/twitter-image.tsx
@@ -1,6 +1,6 @@
 // Twitter cards use the same dimensions
 export const size = { height: 630, width: 1200 };
-export const contentType = "image/webp";
+export const contentType = "image/png";
 
 // Re-use the same OG image for Twitter
 export { default } from "./opengraph-image";
